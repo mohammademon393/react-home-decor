a0bcc0a9a01ffb59397fea64cd88549a5f50fc9b
mohammademon393 <mdemon357ep@gmail.com>
2025-10-07 14:54:05 +0600

    main layout and routeing root

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { createBrowserRouter } from "react-router";
 import Home from '../Pages/Home';
 import Products from '../Pages/Products';
+import MainLayOut from '../Layout/MainLayOut';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Home
-  },
-  {
-    path: "/products",
-    Component: Products
+    Component: MainLayOut,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/products",
+        Component: Products,
+      },
+    ],
   },
 ]);
 
