8e2bd291c279c2e2d771ee8ed88d60e9b77f3c67
mohammademon393 <mdemon357ep@gmail.com>
2025-10-07 17:04:42 +0600

    added a responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is navbar</h1>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <Link to={"/"} className="text-xl font-bold">
+            Home Decor
+          </Link>
         </div>
+        <div className="navbar-center "></div>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1 hidden lg:flex">
+            <li>
+              <NavLink to={"/"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/products"}>Products</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/wishlist"}>Wishlist</NavLink>
+            </li>
+          </ul>
+          <NavLink to={"/products"} className="btn">
+            Visit Shop
+          </NavLink>
+        </div>
+      </div>
     );
 };
 
